5bb50c092eb3988d99824508278cad9ed7dcf6a5
Luis Mendez <16315708+LuisMendez01@users.noreply.github.com>
2018-11-20 20:29:25 -0500

    Initial Commit

---
+++ my-tinder-version/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my-tinder-version
+//
+//  Created by Silvia L Mendez on 11/20/18.
+//  Copyright © 2018 Luis Mendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
